d5503038b829ddf68ce55b06056454642f5a2e58
tatojajo <tatojajanidze@gmail.com>
2024-04-14 00:28:27 +0200

    deploy

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+const todos = require("./routers/todos");
+const users = require("./routers/users");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authJwt = require("./helpers/jwt");
+const cors = require("cors");
+require("dotenv/config");
+
+
+app.use(cors());
+app.options("*", cors());
+app.use(bodyParser.json());
+app.use("/api/users", users);
+app.use(authJwt());
+app.use("/api/todos", todos);
+
+
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "todo",
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+app.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}`);
+});
---
+++ routers/todos.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const router = express.Router();
+const { Todo } = require("../models/todos");
+const mongoose = require("mongoose");
+
+// GET todos
+
+
+router.get("/:userId", async (req, res) => {
+    try {
+      const todoList = await Todo.find({ user: req.params.userId }).sort({
+        createdAt: -1,
+      });
+  
+      if (!todoList) {
+        return res.status(400).send("Todos not found");
+      }
+  
+      res.status(200).send(todoList);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  });
+  
+  // POST todos
+  
+  router.post("/:userId", async (req, res) => {
+    try {
+      let newTodo = new Todo({
+        title: req.body.title,
+        description: req.body.description,
+        user: req.params.userId,
+      });
+  
+      newTodo = await newTodo.save();
+  
+      if (!newTodo) {
+        return res.status(400).send("Todo cannot be created");
+      }
+      res.status(201).send(newTodo);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  });
+  
+  // DELETE todos
+  
+  router.delete("/:id", async (req, res) => {
+    try {
+      if (!mongoose.isValidObjectId(req.params.id)) {
+        return res.status(400).send("Invalid todo Id");
+      }
+      const todo = await Todo.findByIdAndDelete({ _id: req.params.id });
+      if (!todo)
+        return res
+          .status(404)
+          .json({ success: false, message: "Todo not found" });
+  
+      res.status(200).json({ success: true, message: "Todo was deleted" });
+    } catch (error) {
+      return res.status(400).json({ success: false, error: error.message });
+    }
+  });
+  
+  // PUT todos
+  
+  router.put("/:id", async (req, res) => {
+    try {
+      if (!mongoose.isValidObjectId(req.params.id)) {
+        return res.status(400).send("Invalid todo Id");
+      }
+      const todo = await Todo.findByIdAndUpdate(
+        req.params.id,
+        { title: req.body.title, completed: req.body.completed },
+        { new: true }
+      );
+  
+      if (!todo) {
+        return res.status(400).send("Todo cannot be updated");
+      }
+      res.status(201).send(todo);
+    } catch (error) {
+      return res.status(400).json({ success: false, error: error.message });
+    }
+  });
+  
+  module.exports = router;
